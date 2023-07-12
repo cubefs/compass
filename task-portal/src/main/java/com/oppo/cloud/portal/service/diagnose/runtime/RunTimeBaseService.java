890477a151760daead5545e0da9b7fc4a8c234d4
nilnon <wangxiaoyou@oppo.com>
2023-07-12 11:28:23 +0800

    fix: task-portal RunTimeBaseService NPE issue

--- task-portal/src/main/java/com/oppo/cloud/portal/service/diagnose/runtime/RunTimeBaseService.java
+++ task-portal/src/main/java/com/oppo/cloud/portal/service/diagnose/runtime/RunTimeBaseService.java
@@ -83,16 +83,17 @@ public abstract class RunTimeBaseService<T extends IsAbnormal> implements Genera
         Conclusion conclusion = null;
         // es查询元数据
         try {
-            for (DetectorResult detectorResult : detectorStorage.getDataList()) {
-                if (detectorResult.getAppCategory().equals(this.getCategory())) {
-                    // 根据es元数据生成诊断报告
-                    data = this.generateData(detectorResult, detectorStorage.getConfig());
-                    // 根据诊断结果数据中var变量生成诊断建议
-                    conclusion = this.generateConclusion(data);
-                    break;
+            if (detectorStorage.getDataList() != null) {
+                for (DetectorResult detectorResult : detectorStorage.getDataList()) {
+                    if (detectorResult.getAppCategory().equals(this.getCategory())) {
+                        // 根据es元数据生成诊断报告
+                        data = this.generateData(detectorResult, detectorStorage.getConfig());
+                        // 根据诊断结果数据中var变量生成诊断建议
+                        conclusion = this.generateConclusion(data);
+                        break;
+                    }
                 }
             }
-
         } catch (Exception e) {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             e.printStackTrace(new PrintStream(baos));
